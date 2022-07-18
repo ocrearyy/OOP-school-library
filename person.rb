e6cec0ea6a36d95c478060ad0c9367a6ab79b769
oshanedesign <oshane.design@gmail.com>
2022-07-18 18:57:06 -0500

    fixed lints

--- person.rb
+++ person.rb
@@ -7,7 +7,6 @@ class Person
         @name = name 
         @age = age
         @permission_granted = permission_granted
-
     end
 
     private
@@ -26,44 +25,3 @@ class Person
             return false
     end 
 end 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# This a Person class
-class Person
-    attr_accessor :name, :age
-    attr_reader :id
-  
-    def initialize(age, name = 'Unknown', parent_permission: true)
-      @id = Random.rand(1..100)
-      @name = name,
-              @age = age,
-              @parent_permission = parent_permission
-    end
-  
-    def can_use_services?
-      if isof_age? || @parent_permission
-        true
-      else
-        false
-      end
-    end
-  
-    private
-  
-    def isof_age?
-      @age >= 18
-    end
-  end
-
-
