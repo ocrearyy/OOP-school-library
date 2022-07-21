012c40075dc701cb2cea6f8bc783be0271d65072
oshanedesign <oshane.design@gmail.com>
2022-07-21 17:24:53 -0500

    created classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
     @classroom = classroom
     super
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hooky
     returns "¯\(ツ)/¯"
+  end
+
+  def update_classroom(classroom)
+@classroom = classroom
+classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
